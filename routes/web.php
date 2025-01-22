2467fcf707b389ac69396a1c132b84349946f02c
Yurii Kuza <yuramay08@gmail.com>
2025-01-22 23:34:34 +0100

    n... new commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('menu');
 });
+Route::get('/collection', function () {
+    return view('collection');
+})->name('collection');
+
+Route::get('/game', function () {
+    return view('game');
+})->name('game');
+
+Route::get('/login', [AccountController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AccountController::class, 'login']);
+
+Route::get('/register', [AccountController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AccountController::class, 'register']);
