33321bf3b70c71b30e2a5d4e7c089e3541cbf25a
DaliborSeckar <seckardalibor2007@gmail.com>
2025-02-13 13:27:00 +0100

    upratanie, maid

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -8,39 +8,40 @@
   </head>
   <body>
     <div class="hra">
-    <div class="NDiv-3" style="  width: 100%; height: 100%; display: flexbox;">
-      <div class="top-menu">
-        <div class="your-score">
-          <div class="overlap-23">
-            <div class="rectangle-9"></div>
-            <div class="rectangle-10"></div>
-            <div class="text-wrapper-12">0</div>
+      <div class="NDiv-3" style="width: 100%; height: 100%; display: flexbox;">
+        <div class="top-menu">
+          <div class="your-score">
+            <div class="overlap-23">
+              <div class="rectangle-9"></div>
+              <div class="rectangle-10"></div>
+              <div class="text-wrapper-12">0</div>
+            </div>
+          </div>
+          <div class="enemy-score">
+            <div class="overlap-24">
+              <div class="rectangle-11"></div>
+              <div class="rectangle-13"></div>
+              <div class="text-wrapper-13">0</div>
+            </div>
           </div>
         </div>
-        <div class="enemy-score">
-          <div class="overlap-24">
-            <div class="rectangle-11"></div>
-            <div class="rectangle-13"></div>
-            <div class="text-wrapper-13">0</div>
+        <div class="flex-boxGame">
+          <div class="jokers">
+            <div class="img-wrapper">
+              <div class="rectangle-7"></div>
+            </div>
           </div>
-        </div>
-      </div>
-<div class="flex-boxGame">
-<div class="jokers">
-  <div class="img-wrapper"><div class="rectangle-7"></div></div>
-</div>
-  <div class="bottom-menu2">
-    <div>
-      <div class="overlap-15">
-        <div class="bottom-menu">
-          <div class="rectangle-5"></div>
+          <div class="bottom-menu2">
+            <div>
+              <div class="overlap-15">
+                <div class="bottom-menu">
+                  <div class="rectangle-5"></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-</div>
-</div>
-      </div>
-    </div>
-</body>
-
+  </body>
 </html>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -10,75 +10,74 @@
 
 <body>
     <div class="hra">
-            <div class="overlap">
-                <div class="overlap-group">
-                    <div class="rectangle">
-                        <div class="text-wrapper-2">J</div>
-                        <div class="text-wrapper-6">o</div>
-                        <div class="text-wrapper-5">k</div>
-                        <div class="text-wrapper-4">e</div>
-                        <div class="text-wrapper-3">r</div>
+        <div class="overlap">
+            <div class="overlap-group">
+                <div class="rectangle">
+                    <div class="text-wrapper-2">J</div>
+                    <div class="text-wrapper-6">o</div>
+                    <div class="text-wrapper-5">k</div>
+                    <div class="text-wrapper-4">e</div>
+                    <div class="text-wrapper-3">r</div>
+                </div>
+                <div class="jokery">
+                    <div class="joker">
+                        <div class="overlap-2">
+                            <img src="jokerNigga" alt="jokerNigga">
+                        </div>
                     </div>
-<div class="jokery">
-    <div class="joker">
-        <div class="overlap-2">
-            <img src="jokerNigga" alt="jokerNigga">
+                    <div class="joker-2">
+                        <div class="overlap-2">
+                            <img src="jokerNigga" alt="jokerNigga">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="buttons">
+            <div class="top">
+                <a href="{{ route('game') }}" class="collection-button">
+                    <div class="overlap-13">
+                        <div class="group-2">
+                            <div class="rectangle-3">
+                                <div class="text-wrapper-8">Online</div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a href="{{ route('game') }}" class="collection-button">
+                    <div class="overlap-13">
+                        <div class="group-2-2">
+                            <div class="rectangle-3">
+                                <div class="text-wrapper-8-2">Offline</div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="bottom">
+                <a href="{{ route('collection') }}" class="collection-button">
+                    <div class="overlap-13">
+                        <div class="group-2-2-C">
+                            <div class="rectangle-3-C">
+                                <div class="text-wrapper-8-2-C">Collection</div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="top">
+                <a href="{{ route('login') }}" class="collection-button">
+                    <div class="overlap-13">
+                        <div class="group-2">
+                            <div class="rectangle-3">
+                                <div class="text-wrapper-8">Profile</div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
-    <div class="joker-2">
-        <div class="overlap-2">
-            <img src="jokerNigga" alt="jokerNigga">
-        </div>
-    </div>
-</div>
-</div>
-</div>
-<div class="buttons">
-    <div class="top">
-        <a href="{{ route('game') }}" class="collection-button">
-                <div class="overlap-13">
-                    <div class="group-2">
-                        <div class="rectangle-3">
-                            <div class="text-wrapper-8">Online</div>
-                        </div>
-                        </div>
-                </div>
-            </a>
-            <a href="{{ route('game') }}" class="collection-button">
-                <div class="overlap-13">
-                    <div class="group-2-2">
-                        <div class="rectangle-3">
-                            <div class="text-wrapper-8-2">Offline</div>
-                        </div>
-                        </div>
-                </div>
-            </a>
-    </div>
-    <div class="bottom">
-        <a href="{{ route('collection') }}" class="collection-button">
-            <div class="overlap-13">
-                <div class="group-2-2-C">
-                    <div class="rectangle-3-C">
-                        <div class="text-wrapper-8-2-C">Collection</div>
-                    </div>
-                    </div>
-            </div>
-        </a>
-    </div>
-    <div class="top">
-        <a href="{{ route('login') }}" class="collection-button">
-            <div class="overlap-13">
-                <div class="group-2">
-                    <div class="rectangle-3">
-                        <div class="text-wrapper-8">Profile</div>
-                    </div>
-                    </div>
-            </div>
-        </a>
-    </div>
-</div>
-</div>
-</div>
 </body>
 
 </html>
